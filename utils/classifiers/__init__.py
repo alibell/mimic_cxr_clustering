742316bdac8fad842bdc287cdf26a02f62ec1ded
Ali Bellamine <contact@alibellamine.me>
2022-03-29 18:32:41 +0200

    Fix bug

--- utils/classifiers/__init__.py
+++ utils/classifiers/__init__.py
@@ -23,12 +23,19 @@ class ImageClassifier(nn.Module):
     def predict_proba (self, x):
         self.eval()
 
+        if isinstance(x, list):
+            x = x[0]
+
         with torch.no_grad():
             y_hat = self.forward(x)
 
             return y_hat.cpu().numpy()
 
     def predict (self, x):
+
+        if isinstance(x, list):
+            x = x[0]
+
         y_hat = self.predict_proba(x)
 
         return (y_hat >= 0.5)*1
@@ -108,51 +115,3 @@ class naiveImageClassifierMobileNet (ImageClassifier):
 
         return y_hat
 
-# Naive image classifier 
-
-class naiveImageClassifierMobileNet (ImageClassifier):
-
-    def __init__ (self, n_labels=9, weight_balance=True, weights=None):
-        super().__init__(n_labels=n_labels, weight_balance=weight_balance, weights=weights)
-
-       # Getting backbone
-        self.mobilenet = mobilenet_v3_small(pretrained=True)
-
-        # Layer that duplicate the color layer (b&w to rgb)
-        duplicate_color_layers = nn.Conv2d(in_channels=1, out_channels=3, kernel_size=(1,1), padding="same")
-        duplicate_color_layers.requires_grad_(False)
-        duplicate_color_layers.load_state_dict({
-            "weight":torch.ones(duplicate_color_layers.weight.shape, dtype=torch.float32),
-            "bias":torch.zeros(duplicate_color_layers.bias.shape)
-        })
-
-        self.encoder = nn.Sequential(*[
-            duplicate_color_layers,
-            nn.Sequential(*list(self.mobilenet.features.children()))[0:-3],
-            nn.Conv2d(96, 48, (3,3), padding="same"),
-            nn.BatchNorm2d(48),
-        ])
-
-        self.classification_layer = nn.Sequential(
-            nn.Conv2d(48, 96, (3,3), padding="same"),
-            nn.BatchNorm2d(96),
-            nn.ReLU(),
-            nn.Conv2d(96, 256, (3,3), padding="same"),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.AdaptiveAvgPool2d((2,2)),
-            nn.Flatten(),
-            nn.Linear(1024, 512),
-            nn.ReLU(),
-            nn.Linear(512, 32),
-            nn.ReLU(),
-            nn.Linear(32, n_labels),
-            nn.Sigmoid()
-        )
-        self.optimizer = optim.Adam(self.parameters(), lr=1e-4)
-
-    def forward (self, x):
-        y_hat = self.encoder(x)
-        y_hat = self.classification_layer(y_hat)
-
-        return y_hat
